cb43e1df91db5949fad26e504024fec95d849822
Nsreen Miri <nsreenmiri@gmail.com>
2023-12-18 13:42:42 +0100

    Update KafkaProducer.py

--- Online-Stage/Kafka/KafkaProducer.py
+++ Online-Stage/Kafka/KafkaProducer.py
@@ -2,48 +2,56 @@ from confluent_kafka import Producer
 import json
 import csv
 import time
-import datetime
+from datetime import datetime
 import os
 
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+
 def send_data_to_kafka(bootstrap_servers, topic, data_path):
-    # Load the data
     print(f'Loading data from {data_path}')
     with open(data_path, 'r') as csv_file:
-        data = csv.DictReader(csv_file)
-        
+        data = list(csv.DictReader(csv_file))
+
         # Initialize producer
         producer = Producer({'bootstrap.servers': bootstrap_servers})
-        
-        # Time interval
-        startTime = time.time()
-        waitSeconds = 0.1
 
-        for row in data:
-            #print(row)
-            # Convert to a JSON format
-            current_time = time.localtime()
-            #print(current_time)
-            old_time = datetime.datetime.fromisoformat(row['Timestamp'])
-            #print(old_time)
-            new_time = old_time + datetime.timedelta(seconds=107209960)
-            #print(new_time)
-            row['Timestamp'] = new_time.isoformat(' ')
-            #print(row['Timestamp'])
-            payload = json.dumps(row)
-            
-            # Produce
-            producer.produce(topic=topic, value=payload.encode('utf-8'))
-            
-            # Wait a number of seconds until the next message
-            time.sleep(.5)
+        total_rows = len(data)
+        print(f"Total rows to send: {total_rows}")
+
+        for index, row in enumerate(data):
+            try:
+                # Convert to a datetime object
+                dt_object = datetime.fromisoformat(row['Timestamp'])
+
+                # Convert datetime to milliseconds since epoch
+                current_timestamp_ms = int(time.time() * 1000)
+                row['Timestamp'] = current_timestamp_ms
+                row["PowerFlowValue"]=float(row["PowerFlowValue"])
+                row["PowerLineID"]=float(row["PowerLineID"])
+                payload = json.dumps(row)
+
+                # Produce
+                producer.produce(topic=topic, value=payload.encode('utf-8'), callback=delivery_report)
+                producer.poll(0)
+                time.sleep(5)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+            # Progress bar update and sleep omitted for brevity
 
         producer.flush()
-        print(f"Emptied {data_path}")
+        print(f"\nFinished sending data from {data_path}")
 
 if __name__ == "__main__":
-    BOOTSTRAP_SERVER = 'localhost:9092'
-    TOPIC = 'Epsymolo'
-    DATA_DIR = 'Data'  # Update this to the directory containing your CSV files
+    BOOTSTRAP_SERVER = 'kafka:9092'  # Replace with your actual bootstrap server
+    TOPIC = 'Epsymolo'  # Replace with your actual topic
+    DATA_DIR = 'Data'  # Replace with your actual data directory
 
     # Iterate through all CSV files in the specified directory
     for filename in os.listdir(DATA_DIR):
